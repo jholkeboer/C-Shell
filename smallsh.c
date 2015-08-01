7a51842932e40e25189e9b475c58756f5d3be1d8
Jack Holkeboer <holkeboj@eos-class.engr.oregonstate.edu>
2015-08-01 12:54:29 -0700

    initial commit

---
+++ smallsh.c
@@ -0,0 +1,22 @@
+// Jack Holkeboer
+// Oregon State CS344
+// August 3st, 2015
+// holkeboj@onid.oregonstate.edu
+// Program 3: smallsh
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+void loopshell(void);
+
+int main(int argc, char **argv) {
+	loopshell();
+	return 0;
+}
+
+void loopshell(void) {
+	printf("Testing\n");
+}
